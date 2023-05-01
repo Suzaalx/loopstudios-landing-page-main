0d9f09c2aba76061cf96008dc1f6ec82fe926247
Suzaalx <sujalxetry00@gmail.com>
2023-05-01 21:53:22 +0545

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+
+    fontFamily: {
+      Alata: ["Alata", "sans-serif"],
+      Josefin: ["Josefin Sans", "sans-serif"],
+      Indie : ["Indie Flower", "cursive"],
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
